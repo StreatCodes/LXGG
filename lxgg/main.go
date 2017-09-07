314b1d184b92d1211fa0e25e836575b0214017a3
StreatCodes <matt.r.streatfield@gmail.com>
2017-09-07 22:27:48 +1000

    Serve static files

--- lxgg/main.go
+++ lxgg/main.go
@@ -1,7 +1,10 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
+	"os"
+	"path/filepath"
 
 	"github.com/go-chi/chi"
 	"github.com/go-chi/chi/middleware"
@@ -10,7 +13,7 @@ import (
 func main() {
 	r := chi.NewRouter()
 
-	r.Get("/", rootHandler)
+	// r.Get("/", rootHandler)
 	r.Post("/login", loginHandler)
 
 	r.Route("/api", func(r chi.Router) {
@@ -23,9 +26,28 @@ func main() {
 		r.Get("/containers", containersAllHandler)
 	})
 
+	workDir, _ := os.Getwd()
+	filesDir := filepath.Join(workDir, "/static")
+	staticServer(r, "/", http.Dir(filesDir))
+
 	http.ListenAndServe(":3001", r)
 }
 
 func rootHandler(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("<html><body>Welcome to the LXGG api, hit up <a href=\"https://github.com/streatcodes/lxgg\">our github page for docs!</a></body></html>"))
+}
+
+func staticServer(r chi.Router, path string, root http.FileSystem) {
+	fs := http.StripPrefix(path, http.FileServer(root))
+
+	if path != "/" && path[len(path)-1] != '/' {
+		r.Get(path, http.RedirectHandler(path+"/", 301).ServeHTTP)
+		path += "/"
+	}
+	path += "*"
+
+	r.Get(path, http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		fmt.Println(r.RequestURI)
+		fs.ServeHTTP(w, r)
+	}))
 }
