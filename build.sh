218371d9d9ac69436b6a07b0d0229940fc80a1f5
StreatCodes <matt.r.streatfield@gmail.com>
2017-09-12 10:10:41 +0000

    Write build script

--- build.sh
+++ build.sh
@@ -1,2 +1,16 @@
 #!/bin/bash
-echo TODO
+echo Build server
+pushd lxgg;
+go build .;
+popd;
+pushd lxgg_frontend;
+npm run prod;
+popd;
+rm -rf build/;
+mkdir build;
+mkdir build/static;
+cp lxgg/lxgg build;
+cp -R lxgg_frontend/css build/static/;
+cp -R lxgg_frontend/icons build/static/;
+cp -R lxgg_frontend/index.html build/static/;
+cp -R lxgg_frontend/js build/static/;
